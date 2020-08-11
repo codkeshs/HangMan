2e743b4609d6d164c01eb3ececeead5e59b54b9b
shayan <malekishayan79@gmail.com>
2020-08-11 15:05:58 +0430

    fix image address

--- src/main/java/ir/codekeshs/Game.java
+++ src/main/java/ir/codekeshs/Game.java
@@ -3,11 +3,14 @@ package ir.codekeshs;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,13 +33,18 @@ public class Game {
         SceneParent.getStage().setScene(new Scene(root, 800, 600));
         Label label = new Label(chosen);
         root.getChildren().add(label);
-        guess(SceneParent.getStage().getScene());
+        try {
+            guess(SceneParent.getStage().getScene());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void guess(Scene scene) {
+    public void guess(Scene scene) throws IOException {
         HBox hBox = new HBox();
         for (int i = 0; i < answer.length(); i++) {
-            hBox.getChildren().add(new StackPane(new ImageView("file:/home/shayan/Downloads/orange.png")));
+            hBox.getChildren().add(new StackPane(new ImageView(new Image(new FileInputStream(
+                    "src/main/resources/game/orange.png")))));
         }
         root.setCenter(hBox);
         hBox.setAlignment(Pos.CENTER);
